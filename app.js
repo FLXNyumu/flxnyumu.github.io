e9225e78cd6ab639c371b0cb0577931cf0ed273d
FLXNyumu <100859284+FLXNyumu@users.noreply.github.com>
2022-03-03 19:05:21 +0300

    All Files

---
+++ app.js
@@ -0,0 +1,9 @@
+
+    const bars = document.querySelectorAll('.progress_bar');
+    bars.forEach(function(bar){
+        let percentage = bar.dataset.percent;
+        let tooltip = bar.children[0];
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
+        console.log(percentage);
+    });
